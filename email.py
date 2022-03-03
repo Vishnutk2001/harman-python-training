b39b208fae428ee1462ec23bc4ecb1cb842cb17d
Vishnutk2001 <t.k.vishnu2001@gmail.com>
2022-03-03 16:17:16 +0530

    mailes

---
+++ email.py
@@ -0,0 +1,9 @@
+namelist = []
+emaillist = []
+for i in range(0,5):
+    name = input("enter a name")
+    email = input("enter email")
+    namelist.append(name)
+    emaillist.append(email)
+print(namelist)
+print(emaillist)
